b37d6867072b9e462a2ff6e452271f8ef9a4dbb8
AlexMaHao <zziamahao@163.com>
2016-03-23 21:16:35 +0800

    one

---
+++ app/src/main/java/com/mahao/xrzdemo/ui/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.mahao.xrzdemo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ *
+ * Created by Alex_MaHao on 2016/3/22.
+ */
+public class BaseActivity  extends AppCompatActivity{
+
+
+
+}
